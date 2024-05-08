0680af903d872398fe830d3453cac56926577a75
gelu <2723579248@qq.com>
2024-05-08 10:48:47 +0800

    test(): 测试sendMessage方法

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+// TestLocalTransport_Connect
+//
+//	@Description: 测试LocalTransport的Connect方法
+//	@param t
 func TestLocalTransport_Connect(t *testing.T) {
 	tra := NewLocalTransport("A")
 	trb := NewLocalTransport("B")
@@ -14,4 +18,27 @@ func TestLocalTransport_Connect(t *testing.T) {
 
 	assert.Equal(t, tra.peers[trb.addr], trb)
 	assert.Equal(t, trb.peers[tra.addr], tra)
+}
+
+// TestLocalTransport_SendMessage
+//
+//	@Description: 测试LocalTransport的SendMessage方法
+//	@param t
+func TestLocalTransport_SendMessage(t *testing.T) {
+	tra := NewLocalTransport("A")
+	trb := NewLocalTransport("B")
+	tra.Connect(trb)
+	trb.Connect(tra)
+	msg := []byte("hello")
+	// 显式启动一个goroutine来消费trb的消息
+	go func() {
+		for {
+			rpc := <-trb.Consume()
+			// 这里可以添加处理消息的逻辑，或者简单记录消息以确保通道不被阻塞
+			assert.Equal(t, rpc.Payload, msg)
+			assert.Equal(t, rpc.From, tra.Addr())
+		}
+	}()
+
+	assert.Nil(t, tra.SendMessage(trb.Addr(), msg))
 }
--- network/transport.go
+++ network/transport.go
@@ -11,22 +11,22 @@ type RPC struct {
 //
 //	@Description: 传输
 type Transport interface {
-	//
+	// Consume
 	//  Consume(RPC)
 	//  @Description:从RPC（Remote Procedure Call，远程过程调用）到RPC的通道
 	Consume(RPC) <-chan RPC
 
-	//
+	// Connect
 	//  Connect(Transport)
 	//  @Description: 连接到另一个Transport实例
 	Connect(Transport) error
 
-	//
+	// SendMessage
 	//  SendMessage(NetAddr, []byte)
 	//  @Description: 发送一个字节切片（通常代表消息或数据）到指定的NetAddr（网络地址）
 	SendMessage(NetAddr, []byte) error
 
-	//
+	// Addr
 	//  Addr()
 	//  @Description:当前Transport的NetAddr
 	Addr() NetAddr
